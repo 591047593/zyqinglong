b82fa7e0195f34a3fdddaa72c24910ab4748d3ef
591047593 <591047593@qq.com>
2025-01-18 21:23:42 +0800

    创建 SFSY@2025.py

---
+++ SFSY@2025.py
@@ -0,0 +1,68 @@
+#多号换行，变量名：sfsyUrl
+# const $ = new Env('顺丰速运')
+import hashlib
+import json
+import os
+import random
+import time
+from datetime import datetime, timedelta
+from sys import exit
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# 禁用安全请求警告
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+IS_DEV = False
+if os.path.isfile('DEV_ENV.py'):
+    import DEV_ENV
+
+    IS_DEV = True
+
+
+#from notify import send
+
+send_msg = ''
+one_msg = ''
+
+
+def Log(cont=''):
+    global send_msg, one_msg
+    print(cont)
+    if cont:
+        one_msg += f'{cont}\n'
+        send_msg += f'{cont}\n'
+
+
+inviteId = ['']
+
+
+class RUN:
+    def __init__(self, info, index):
+        global one_msg
+        one_msg = ''
+        split_info = info.split('@')
+        url = split_info[0]
+        len_split_info = len(split_info)
+        last_info = split_info[len_split_info - 1]
+        self.send_UID = None
+        if len_split_info > 0 and "UID_" in last_info:
+            self.send_UID = last_info
+        self.index = index + 1
+        Log(f"\n---------开始执行第{self.index}个账号>>>>>")
+        self.s = requests.session()
+        self.s.verify = False
+        self.headers = {
+            'Host': 'mcs-mimp-web.sf-express.com',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x63090551) XWEB/6945 Flue',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'sec-fetch-site': 'none',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-user': '?1',
+            'sec-fetch-dest': 'document',
+            'accept-language': 'zh-CN,zh',
+            'platform': 'MINI_PROGRAM',
+
+        }
+        self.anniversary_black = False
