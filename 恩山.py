ba00604c94fc8783480745a5b7f330411d6556b0
591047593 <591047593@qq.com>
2025-03-23 01:09:29 +0800

    更新 恩山.py

--- 恩山.py
+++ 恩山.py
@@ -1,68 +1,37 @@
+#!/usr/bin/python3
+# -- coding: utf-8 -- 
+# -------------------------------
+# @Author : github@wd210010 https://github.com/wd210010/only_for_happly
+# @Time : 2023/10/4 16:23
+# -------------------------------
+# cron "0 0 2 * * *" script-path=xxx.py,tag=匹配cron用
+# const $ = new Env('恩山签到')
+import requests,re,os
 
-import json
-import os
-import re
+#配置恩山的cookie 到配置文件config.sh export enshanck='' 需要推送配置推送加token export plustoken=''
+enshanck = os.getenv("enshanck")
 
-import requests
-import urllib3
+#推送加 token
+plustoken = os.getenv("plustoken")
 
-from dailycheckin import CheckIn
+def Push(contents):
+    # 推送加
+    headers = {'Content-Type': 'application/json'}
+    json = {"token": plustoken, 'title': '恩山签到', 'content': contents.replace('\n', '<br>'), "template": "json"}
+    resp = requests.post(f'http://www.pushplus.plus/send', json=json, headers=headers).json()
+    print('push+推送成功' if resp['code'] == 200 else 'push+推送失败')
 
-urllib3.disable_warnings()
-
-
-class EnShan(CheckIn):
-    name = "恩山无线论坛"
-
-    def __init__(self, check_item):
-        self.check_item = check_item
-
-    @staticmethod
-    def sign(cookie):
-        msg = []
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.125 Safari/537.36",
-            "Cookie": cookie,
-        }
-        response = requests.get(
-            url="https://www.right.com.cn/FORUM/home.php?mod=spacecp&ac=credit&showcredit=1",
-            headers=headers,
-            verify=False,
-        )
-        try:
-            coin = re.findall("恩山币: </em>(.*?)&nbsp;", response.text)[0]
-            point = re.findall("<em>积分: </em>(.*?)<span", response.text)[0]
-            msg = [
-                {
-                    "name": "恩山币",
-                    "value": coin,
-                },
-                {
-                    "name": "积分",
-                    "value": point,
-                },
-            ]
-        except Exception as e:
-            msg = [
-                {
-                    "name": "签到失败",
-                    "value": str(e),
-                }
-            ]
-        return msg
-
-    def main(self):
-        cookie = self.check_item.get("cookie")
-        msg = self.sign(cookie=cookie)
-        msg = "\n".join([f"{one.get('name')}: {one.get('value')}" for one in msg])
-        return msg
-
-
-if __name__ == "__main__":
-    with open(
-        os.path.join(os.path.dirname(os.path.dirname(__file__)), "config.json"),
-        encoding="utf-8",
-    ) as f:
-        datas = json.loads(f.read())
-    _check_item = datas.get("ENSHAN", [])[0]
-    print(EnShan(check_item=_check_item).main())
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.125 Safari/537.36",
+    "Cookie": enshanck,
+}
+session = requests.session()
+response = session.get('https://www.right.com.cn/FORUM/home.php?mod=spacecp&ac=credit&showcredit=1', headers=headers)
+try:
+    coin = re.findall("恩山币: </em>(.*?)&nbsp;", response.text)[0]
+    point = re.findall("<em>积分: </em>(.*?)<span", response.text)[0]
+    res = f"恩山币：{coin}\n积分：{point}"
+    print(res)
+    Push(contents=res)
+except Exception as e:
+    res = str(e)
